0fb28f4e63c7160df0af177bff204b9a00901e5c
sunhao <1301452387@qq.com>
2022-06-09 11:20:00 +0800

    third commit

--- src/main/java/com/sunhao/demo1.java
+++ src/main/java/com/sunhao/demo1.java
@@ -7,5 +7,6 @@ public class demo1 {
     public static void main(String[] args) {
         System.out.println("第一次测试");
         System.out.println("第二次测试");
+        System.out.println("第三次测试");
     }
 }
